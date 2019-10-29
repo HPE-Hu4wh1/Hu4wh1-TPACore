183b803ce67ff724a5bcf482535a60b2be480561
Aldo <57144729+HPE-Hu4wh1@users.noreply.github.com>
2019-10-29 11:09:43 -0500

    Completed.

---
+++ src/Hu4wh1/Commands/Tpar.php
@@ -0,0 +1,56 @@
+<?php
+/*
+  _____  ____    _     ____  
+ |_   _||  _ \  / \   |  _ \ 
+   | |  | |_) |/ _ \  | |_) |
+   | |  |  __// ___ \ |  _ < 
+   |_|  |_|  /_/   \_\|_| \_\
+                       
+*/                            
+namespace Hu4wh1\Commands;
+
+use Hu4wh1\BaseAPI;
+
+use pocketmine\command\Command;
+
+use pocketmine\command\CommandSender;
+
+use pocketmine\Player;
+
+use pocketmine\utils\TextFormat;
+
+class Tpar extends Command{
+
+    /** @var BaseAPI */
+
+    private $plugin;
+
+    public function __construct(){
+
+    $this->plugin = BaseAPI::getInstance();
+
+    parent::__construct("tpar", "Tpar command!");
+
+    $this->setPermission("essentials.tpar.commands");
+
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args) : bool{
+
+      if(!$this->testPermission($sender)) return false;
+
+        if(!empty($args[0])){
+
+            $sender->sendMessage(TextFormat::RED."Use: /tpak");
+
+        }else{
+
+            $this->plugin->tpar($sender->getName());
+
+        }
+
+     return true;
+
+   }
+
+}
